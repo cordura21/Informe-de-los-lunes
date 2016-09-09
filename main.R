343d03e8c6d60d47d86068a2ac429e5dbeebbafb
cordura21 <cordura21@gmail.com>
2016-09-09 14:58:26 -0300

    added risk ratios

--- main.R
+++ main.R
@@ -24,9 +24,24 @@ my.data$pnl$PorcRentDiaria <- my.data$pnl$PorcRentDiaria / 100
 risk.dict <- read.csv('dictionary_risk.csv', stringsAsFactors = FALSE) %>% tbl_df() %>%
   mutate(risk.type = paste(position,Risk.Type,sep = ' | '))
 
-my.data$bulk <- merge(my.data$bulk,risk.dict,by = 'AT12') %>% tbl_df()
 
 # Additional calculations -------------------------------------------------
+
+risk.calcs <- my.data$bulk %>%
+  select(Fecha,Valuacion,Exposure,Margin,
+         LongExposure,ShortExposure) %>%
+  group_by(Fecha) %>%
+  summarise_all(.funs = sum)
+
+real.margin <- my.data$bulk %>%
+  filter(AT12 == "Cash Margin Accounts") %>%
+  select(Fecha,Valuacion) %>%
+  group_by(Fecha) %>%
+  summarise_all(.funs = sum)
+names(real.margin) <- c('Fecha','Real.Margin')
+risk.calcs <- merge(risk.calcs,real.margin, by = 'Fecha') %>% tbl_df() %>%
+  mutate(leverage.ratio = Exposure / Valuacion,
+         margin.ratio = Real.Margin / Valuacion)
 
 # Dates
 library(lubridate)
@@ -47,133 +62,105 @@ year(my.dates$twelve.months) <- as.numeric(year(my.dates$twelve.months))-1
 fees <- list(performance = 14/100)
 
 # summary -------------------------------------------------
-my.tables <- list()
 
-# Portafolio Total
+add.summary.table <- function(x,familia, start.date, date.label) {
+  require(dplyr)
+  my.object <- tbl_df(x)
+  
+  
+  my.object <- my.data$pnl %>%
+    filter(NombreFamilia == familia) %>%
+    filter(Fecha >= start.date) %>%
+    group_by(NombreFamilia) %>%
+    summarize(retorno = sum(exp(log(PorcRentDiaria+1))-1),
+              patrimonio.inicial = first(PatFamiliaFinal),
+              patrimonio.Final = last(PatFamiliaFinal),
+              patrimonio.promedio = mean(PatFamiliaFinal),
+              resultado = sum(RentDiaria),
+              cashflows = sum(Suscripciones) - sum(Rescates)) %>%
+    mutate(retorno.neto.est = ifelse(retorno > 0 ,
+                                     retorno * (1-fees$performance), retorno),
+           periodo = date.label)
+  my.object <- cbind(my.object$periodo,my.object)
+  my.object[,1] <-  my.object$periodo
+  my.object$periodo <- NULL
+  names(my.object)[1] <- 'periodo'
+  
+  return(my.object)
+  
+}
 
-my.tables$mtd.portafolio <- my.data$pnl %>%
-  filter(NombreFamilia == "TOTAL T+ L") %>%
-  filter(Fecha >= my.dates$mtd) %>%
-  group_by(NombreFamilia) %>%
-  summarize(retorno = sum(log(PorcRentDiaria+1)),
-            patrimonio.inicial = first(PatFamiliaFinal),
-            patrimonio.Final = last(PatFamiliaFinal),
-            resultado = sum(RentDiaria),
-            cashflows = sum(Suscripciones) - sum(Rescates)) %>%
-  mutate(retorno.neto.est = ifelse(retorno > 0 ,
-                        retorno * (1-fees$performance), retorno),
-         periodo = 'mtd')
+my.tables <- add.summary.table(x = my.data$pnl,familia = 'TOTAL T+ L',
+                               start.date = my.dates$current,date.label = 'Daily')
 
-my.tables$ytd.portafolio <- my.data$pnl %>%
-  filter(NombreFamilia == "TOTAL T+ L") %>%
-  filter(Fecha >= my.dates$ytd) %>%
-  group_by(NombreFamilia) %>%
-  summarize(retorno = sum(log(PorcRentDiaria+1)),
-            patrimonio.inicial = first(PatFamiliaFinal),
-            patrimonio.Final = last(PatFamiliaFinal),
-            resultado = sum(RentDiaria),
-            cashflows = sum(Suscripciones) - sum(Rescates)) %>%
-  mutate(retorno.neto.est = ifelse(retorno > 0 ,
-                                   retorno * (1-fees$performance), retorno),
-         periodo = 'ytd')
+my.tables <- rbind(my.tables,
+                   add.summary.table(x = my.data$pnl,familia = 'TOTAL T+ L',
+                                     start.date = my.dates$mtd,date.label = 'MTD'))
+my.tables <- rbind(my.tables,
+                   add.summary.table(x = my.data$pnl,familia = 'TOTAL T+ L',
+                                     start.date = my.dates$ytd,date.label = 'YTD'))
 
-my.tables$twelve.months.portafolio <- my.data$pnl %>%
-  filter(NombreFamilia == "TOTAL T+ L") %>%
-  filter(Fecha >= my.dates$twelve.months) %>%
-  group_by(NombreFamilia) %>%
-  summarize(retorno = sum(log(PorcRentDiaria+1)),
-            patrimonio.inicial = first(PatFamiliaFinal),
-            patrimonio.Final = last(PatFamiliaFinal),
-            resultado = sum(RentDiaria),
-            cashflows = sum(Suscripciones) - sum(Rescates)) %>%
-  mutate(retorno.neto.est = ifelse(retorno > 0 ,
-                                   retorno * (1-fees$performance), retorno),
-         periodo = '12 meses')
+my.tables <- rbind(my.tables,
+                   add.summary.table(x = my.data$pnl,familia = 'TOTAL T+ L',
+                                     start.date = my.dates$twelve.months,date.label = '12 meses'))
 
-# Futuros Administrados
 
-my.tables$mtd.Alfa <- my.data$pnl %>%
-  filter(NombreFamilia == "Alfa Total") %>%
-  filter(Fecha >= my.dates$mtd) %>%
-  group_by(NombreFamilia) %>%
-  summarize(retorno = sum(log(PorcRentDiaria+1)),
-            patrimonio.inicial = first(PatFamiliaFinal),
-            patrimonio.Final = last(PatFamiliaFinal),
-            resultado = sum(RentDiaria),
-            cashflows = sum(Suscripciones) - sum(Rescates)) %>%
-  mutate(retorno.neto.est = ifelse(retorno > 0 ,
-                                   retorno * (1-fees$performance), retorno),
-         periodo = 'mtd')
+my.tables <- rbind(my.tables,
+                   add.summary.table(x = my.data$pnl,familia = 'Alfa Total',
+                                     start.date = my.dates$current,date.label = 'Daily'))
+my.tables <- rbind(my.tables,
+                   add.summary.table(x = my.data$pnl,familia = 'Alfa Total',
+                                     start.date = my.dates$mtd,date.label = 'MTD'))
+my.tables <- rbind(my.tables,
+                   add.summary.table(x = my.data$pnl,familia = 'Alfa Total',
+                                     start.date = my.dates$ytd,date.label = 'YTD'))
+my.tables <- rbind(my.tables,
+                   add.summary.table(x = my.data$pnl,familia = 'Alfa Total',
+                                     start.date = my.dates$twelve.months,date.label = '12 meses'))
 
-my.tables$ytd.Alfa <- my.data$pnl %>%
-  filter(NombreFamilia == "Alfa Total") %>%
-  filter(Fecha >= my.dates$ytd) %>%
-  group_by(NombreFamilia) %>%
-  summarize(retorno = sum(log(PorcRentDiaria+1)),
-            patrimonio.inicial = first(PatFamiliaFinal),
-            patrimonio.Final = last(PatFamiliaFinal),
-            resultado = sum(RentDiaria),
-            cashflows = sum(Suscripciones) - sum(Rescates)) %>%
-  mutate(retorno.neto.est = ifelse(retorno > 0 ,
-                                   retorno * (1-fees$performance), retorno),
-         periodo = 'ytd')
+my.tables <- rbind(my.tables,
+                   add.summary.table(x = my.data$pnl,familia = 'BETA',
+                                     start.date = my.dates$current,date.label = 'Daily'))
+my.tables <- rbind(my.tables,
+                   add.summary.table(x = my.data$pnl,familia = 'BETA',
+                                     start.date = my.dates$mtd,date.label = 'MTD'))
+my.tables <- rbind(my.tables,
+                   add.summary.table(x = my.data$pnl,familia = 'BETA',
+                                     start.date = my.dates$ytd,date.label = 'YTD'))
+my.tables <- rbind(my.tables,
+                   add.summary.table(x = my.data$pnl,familia = 'BETA',
+                                     start.date = my.dates$twelve.months,date.label = '12 meses'))
 
-my.tables$twelve.months.Alfa <- my.data$pnl %>%
-  filter(NombreFamilia == "Alfa Total") %>%
-  filter(Fecha >= my.dates$twelve.months) %>%
-  group_by(NombreFamilia) %>%
-  summarize(retorno = sum(log(PorcRentDiaria+1)),
-            patrimonio.inicial = first(PatFamiliaFinal),
-            patrimonio.Final = last(PatFamiliaFinal),
-            resultado = sum(RentDiaria),
-            cashflows = sum(Suscripciones) - sum(Rescates)) %>%
-  mutate(retorno.neto.est = ifelse(retorno > 0 ,
-                                   retorno * (1-fees$performance), retorno),
-         periodo = '12 meses')
+my.tables <- rbind(my.tables,
+                   add.summary.table(x = my.data$pnl,familia = 'TESORERIA',
+                                     start.date = my.dates$current,date.label = 'Daily'))
+my.tables <- rbind(my.tables,
+                   add.summary.table(x = my.data$pnl,familia = 'TESORERIA',
+                                     start.date = my.dates$mtd,date.label = 'MTD'))
+my.tables <- rbind(my.tables,
+                   add.summary.table(x = my.data$pnl,familia = 'TESORERIA',
+                                     start.date = my.dates$ytd,date.label = 'YTD'))
+my.tables <- rbind(my.tables,
+                   add.summary.table(x = my.data$pnl,familia = 'TESORERIA',
+                                     start.date = my.dates$twelve.months,date.label = '12 meses'))
 
-# Mercado
+my.tables <- rbind(my.tables,
+                   add.summary.table(x = my.data$pnl,familia = 'FUTUROS ADMINISTRADOS',
+                                     start.date = my.dates$current,date.label = 'Daily'))
+my.tables <- rbind(my.tables,
+                   add.summary.table(x = my.data$pnl,familia = 'FUTUROS ADMINISTRADOS',
+                                     start.date = my.dates$mtd,date.label = 'MTD'))
+my.tables <- rbind(my.tables,
+                   add.summary.table(x = my.data$pnl,familia = 'FUTUROS ADMINISTRADOS',
+                                     start.date = my.dates$ytd,date.label = 'YTD'))
+my.tables <- rbind(my.tables,
+                   add.summary.table(x = my.data$pnl,familia = 'FUTUROS ADMINISTRADOS',
+                                     start.date = my.dates$twelve.months,date.label = '12 meses'))
 
-my.tables$mtd.mercado <- my.data$pnl %>%
-  filter(NombreFamilia == "BETA") %>%
-  filter(Fecha >= my.dates$mtd) %>%
-  group_by(NombreFamilia) %>%
-  summarize(retorno = sum(log(PorcRentDiaria+1)),
-            patrimonio.inicial = first(PatFamiliaFinal),
-            patrimonio.Final = last(PatFamiliaFinal),
-            resultado = sum(RentDiaria),
-            cashflows = sum(Suscripciones) - sum(Rescates)) %>%
-  mutate(retorno.neto.est = ifelse(retorno > 0 ,
-                                   retorno * (1-fees$performance), retorno),
-         periodo = 'mtd')
-
-my.tables$ytd.mercado <- my.data$pnl %>%
-  filter(NombreFamilia == "BETA") %>%
-  filter(Fecha >= my.dates$ytd) %>%
-  group_by(NombreFamilia) %>%
-  summarize(retorno = sum(log(PorcRentDiaria+1)),
-            patrimonio.inicial = first(PatFamiliaFinal),
-            patrimonio.Final = last(PatFamiliaFinal),
-            resultado = sum(RentDiaria),
-            cashflows = sum(Suscripciones) - sum(Rescates)) %>%
-  mutate(retorno.neto.est = ifelse(retorno > 0 ,
-                                   retorno * (1-fees$performance), retorno),
-         periodo = 'ytd')
-
-my.tables$twelve.months.mercado <- my.data$pnl %>%
-  filter(NombreFamilia == "BETA") %>%
-  filter(Fecha >= my.dates$twelve.months) %>%
-  group_by(NombreFamilia) %>%
-  summarize(retorno = sum(log(PorcRentDiaria+1)),
-            patrimonio.inicial = first(PatFamiliaFinal),
-            patrimonio.Final = last(PatFamiliaFinal),
-            resultado = sum(RentDiaria),
-            cashflows = sum(Suscripciones) - sum(Rescates)) %>%
-  mutate(retorno.neto.est = ifelse(retorno > 0 ,
-                                   retorno * (1-fees$performance), retorno),
-         periodo = '12 meses')
 
 library(data.table)
 all.tables <- rbindlist(my.tables)
+all.tables <- cbind(all.tables$periodo,all.tables)
 # visual inspection -------------------------------------------------
 
 # create dates -------------------------------------------------
