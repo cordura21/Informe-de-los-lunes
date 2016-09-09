e0533d8e56cb702e6125d7bbf76cee738c5e3905
cordura21 <cordura21@gmail.com>
2016-09-09 13:33:19 -0300

    Main tables added

--- main.R
+++ main.R
@@ -15,8 +15,11 @@ data.raw <- list(pnl = readRDS(
     file.path(data.path,data.raw.files$familias)) %>% tbl_df()
   )
 # tidy and clean -------------------------------------------------
-names(data.raw$bulk)[which(names(data.raw$bulk) == 'Periodo')] <- 'Fecha'
 my.data <- data.raw
+names(my.data$bulk)[which(names(my.data$bulk) == 'Periodo')] <- 'Fecha'
+my.data$pnl$PorcRentDiaria <- my.data$pnl$PorcRentDiaria / 100
+
+
 # augment data (relational) -------------------------------------------------
 risk.dict <- read.csv('dictionary_risk.csv', stringsAsFactors = FALSE) %>% tbl_df() %>%
   mutate(risk.type = paste(position,Risk.Type,sep = ' | '))
@@ -41,11 +44,136 @@ year(my.dates$twelve.months) <- as.numeric(year(my.dates$twelve.months))-1
 
 
 # Fees
-fees <- list(performance <- 14/100)
-
+fees <- list(performance = 14/100)
 
 # summary -------------------------------------------------
+my.tables <- list()
 
+# Portafolio Total
+
+my.tables$mtd.portafolio <- my.data$pnl %>%
+  filter(NombreFamilia == "TOTAL T+ L") %>%
+  filter(Fecha >= my.dates$mtd) %>%
+  group_by(NombreFamilia) %>%
+  summarize(retorno = sum(log(PorcRentDiaria+1)),
+            patrimonio.inicial = first(PatFamiliaFinal),
+            patrimonio.Final = last(PatFamiliaFinal),
+            resultado = sum(RentDiaria),
+            cashflows = sum(Suscripciones) - sum(Rescates)) %>%
+  mutate(retorno.neto.est = ifelse(retorno > 0 ,
+                        retorno * (1-fees$performance), retorno),
+         periodo = 'mtd')
+
+my.tables$ytd.portafolio <- my.data$pnl %>%
+  filter(NombreFamilia == "TOTAL T+ L") %>%
+  filter(Fecha >= my.dates$ytd) %>%
+  group_by(NombreFamilia) %>%
+  summarize(retorno = sum(log(PorcRentDiaria+1)),
+            patrimonio.inicial = first(PatFamiliaFinal),
+            patrimonio.Final = last(PatFamiliaFinal),
+            resultado = sum(RentDiaria),
+            cashflows = sum(Suscripciones) - sum(Rescates)) %>%
+  mutate(retorno.neto.est = ifelse(retorno > 0 ,
+                                   retorno * (1-fees$performance), retorno),
+         periodo = 'ytd')
+
+my.tables$twelve.months.portafolio <- my.data$pnl %>%
+  filter(NombreFamilia == "TOTAL T+ L") %>%
+  filter(Fecha >= my.dates$twelve.months) %>%
+  group_by(NombreFamilia) %>%
+  summarize(retorno = sum(log(PorcRentDiaria+1)),
+            patrimonio.inicial = first(PatFamiliaFinal),
+            patrimonio.Final = last(PatFamiliaFinal),
+            resultado = sum(RentDiaria),
+            cashflows = sum(Suscripciones) - sum(Rescates)) %>%
+  mutate(retorno.neto.est = ifelse(retorno > 0 ,
+                                   retorno * (1-fees$performance), retorno),
+         periodo = '12 meses')
+
+# Futuros Administrados
+
+my.tables$mtd.Alfa <- my.data$pnl %>%
+  filter(NombreFamilia == "Alfa Total") %>%
+  filter(Fecha >= my.dates$mtd) %>%
+  group_by(NombreFamilia) %>%
+  summarize(retorno = sum(log(PorcRentDiaria+1)),
+            patrimonio.inicial = first(PatFamiliaFinal),
+            patrimonio.Final = last(PatFamiliaFinal),
+            resultado = sum(RentDiaria),
+            cashflows = sum(Suscripciones) - sum(Rescates)) %>%
+  mutate(retorno.neto.est = ifelse(retorno > 0 ,
+                                   retorno * (1-fees$performance), retorno),
+         periodo = 'mtd')
+
+my.tables$ytd.Alfa <- my.data$pnl %>%
+  filter(NombreFamilia == "Alfa Total") %>%
+  filter(Fecha >= my.dates$ytd) %>%
+  group_by(NombreFamilia) %>%
+  summarize(retorno = sum(log(PorcRentDiaria+1)),
+            patrimonio.inicial = first(PatFamiliaFinal),
+            patrimonio.Final = last(PatFamiliaFinal),
+            resultado = sum(RentDiaria),
+            cashflows = sum(Suscripciones) - sum(Rescates)) %>%
+  mutate(retorno.neto.est = ifelse(retorno > 0 ,
+                                   retorno * (1-fees$performance), retorno),
+         periodo = 'ytd')
+
+my.tables$twelve.months.Alfa <- my.data$pnl %>%
+  filter(NombreFamilia == "Alfa Total") %>%
+  filter(Fecha >= my.dates$twelve.months) %>%
+  group_by(NombreFamilia) %>%
+  summarize(retorno = sum(log(PorcRentDiaria+1)),
+            patrimonio.inicial = first(PatFamiliaFinal),
+            patrimonio.Final = last(PatFamiliaFinal),
+            resultado = sum(RentDiaria),
+            cashflows = sum(Suscripciones) - sum(Rescates)) %>%
+  mutate(retorno.neto.est = ifelse(retorno > 0 ,
+                                   retorno * (1-fees$performance), retorno),
+         periodo = '12 meses')
+
+# Mercado
+
+my.tables$mtd.mercado <- my.data$pnl %>%
+  filter(NombreFamilia == "BETA") %>%
+  filter(Fecha >= my.dates$mtd) %>%
+  group_by(NombreFamilia) %>%
+  summarize(retorno = sum(log(PorcRentDiaria+1)),
+            patrimonio.inicial = first(PatFamiliaFinal),
+            patrimonio.Final = last(PatFamiliaFinal),
+            resultado = sum(RentDiaria),
+            cashflows = sum(Suscripciones) - sum(Rescates)) %>%
+  mutate(retorno.neto.est = ifelse(retorno > 0 ,
+                                   retorno * (1-fees$performance), retorno),
+         periodo = 'mtd')
+
+my.tables$ytd.mercado <- my.data$pnl %>%
+  filter(NombreFamilia == "BETA") %>%
+  filter(Fecha >= my.dates$ytd) %>%
+  group_by(NombreFamilia) %>%
+  summarize(retorno = sum(log(PorcRentDiaria+1)),
+            patrimonio.inicial = first(PatFamiliaFinal),
+            patrimonio.Final = last(PatFamiliaFinal),
+            resultado = sum(RentDiaria),
+            cashflows = sum(Suscripciones) - sum(Rescates)) %>%
+  mutate(retorno.neto.est = ifelse(retorno > 0 ,
+                                   retorno * (1-fees$performance), retorno),
+         periodo = 'ytd')
+
+my.tables$twelve.months.mercado <- my.data$pnl %>%
+  filter(NombreFamilia == "BETA") %>%
+  filter(Fecha >= my.dates$twelve.months) %>%
+  group_by(NombreFamilia) %>%
+  summarize(retorno = sum(log(PorcRentDiaria+1)),
+            patrimonio.inicial = first(PatFamiliaFinal),
+            patrimonio.Final = last(PatFamiliaFinal),
+            resultado = sum(RentDiaria),
+            cashflows = sum(Suscripciones) - sum(Rescates)) %>%
+  mutate(retorno.neto.est = ifelse(retorno > 0 ,
+                                   retorno * (1-fees$performance), retorno),
+         periodo = '12 meses')
+
+library(data.table)
+all.tables <- rbindlist(my.tables)
 # visual inspection -------------------------------------------------
 
 # create dates -------------------------------------------------
